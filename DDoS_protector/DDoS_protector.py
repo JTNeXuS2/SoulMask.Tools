1ff5208d6175d5e47f783b4fe2aee16e9415c0c3
⎛⎝Иллидан⎠⎞ <88918931+JTNeXuS2@users.noreply.github.com>
2024-07-11 17:35:49 +0200

    Update DDoS_protector.py

--- DDoS_protector/DDoS_protector.py
+++ DDoS_protector/DDoS_protector.py
@@ -5,7 +5,7 @@ import asyncio
 import os
 import sqlite3
 
-log_file = 'WS.log'
+log_file = 'C:/wgsm/servers/2/serverfiles/WS/Saved/Logs/WS.log'
 
 bans_db = 'bans.db'
 # trigger threshold, how many times an IP address must be found in (NotifyAcceptingConnection accepted) before it is included in the ban list
@@ -46,14 +46,14 @@ async def add_and_block_ip(ip, conn):
         # Execute the command to block the IP address using subprocess if needed
         subprocess.run(f'cmd.exe /c netsh advfirewall firewall add rule name="Block Specific IP" dir=in action=block remoteip={ip}', shell=True)
 
-async def add_ip_name(player_address, player_name, conn):
+async def add_ip_name(player_address, player_name, player_steam, conn):
     #cursor = conn.cursor()
     #cursor.execute("SELECT * FROM ip_address WHERE address=?", (ip,))
     #white = cursor.fetchone()
     #if not white:
-    print(f"IP: {player_address}, Name: {player_name}")
+    print(f"Add IP: {player_address}, Name: {player_name} SteamID: {player_steam}")
     cursor = conn.cursor()
-    cursor.execute("INSERT OR REPLACE INTO ip_address (player_name, address) VALUES (?, ?)", (player_name, player_address))
+    cursor.execute("INSERT OR REPLACE INTO ip_address (player_name, address, steam_id) VALUES (?, ?, ?)", (player_name, player_address, player_steam))
     conn.commit()
 
 async def process_log_file(conn):
@@ -65,8 +65,8 @@ async def process_log_file(conn):
         #ip_pattern = re.compile(r'\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}\b')  # All strings
         ip_pattern = re.compile(r'LogNet: NotifyAcceptingConnection accepted from: (\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}\b)')  # strings only prefix
         # Паттерн для извлечения IP-адреса и имени
-        name_pattern = r"Addr:(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}),.*Name:(\w+)"
-
+        #name_pattern = r"Addr:(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}),.*Name:(\w+)"
+        name_pattern = r"Addr:(\d+\.\d+\.\d+\.\d+),\sNetuid:(\d+),\sName:([\w]+)"
         for line in file:
             match = ip_pattern.search(line)
             if match:
@@ -76,12 +76,13 @@ async def process_log_file(conn):
             matches = re.search(name_pattern, line)
             if matches:
                 player_address = matches.group(1)
-                player_name = matches.group(2)
+                player_steam = matches.group(2)
+                player_name = matches.group(3)
 
                 cursor.execute("SELECT * FROM ip_address WHERE address=?", (player_address,))
                 existing_ban = cursor.fetchone()
                 if not existing_ban:
-                    await add_ip_name(player_address, player_name, conn)
+                    await add_ip_name(player_address, player_name, player_steam, conn)
 
         for ip, count in ip_counts.items():
             if count > sensitive:
@@ -111,12 +112,30 @@ async def tread2(conn):
             print(f"An error: {e}")
         await asyncio.sleep(cooldown)
 
+async def ensure_table_schema(conn):
+    try:
+        cursor = conn.cursor()
+        columns_to_check = {
+            'time': 'TEXT',
+            'address': 'TEXT UNIQUE',
+            'white': 'TEXT',
+            'player_name': 'TEXT',
+            'steam_id': 'TEXT'
+        }
+        cursor.execute('CREATE TABLE IF NOT EXISTS ip_address ({})'.format(', '.join([f'{column} {datatype}' for column, datatype in columns_to_check.items()])))
+
+        cursor.execute('''PRAGMA table_info(ip_address)''')
+        existing_columns = {row[1] for row in cursor.fetchall()}
+        columns_to_add = {column: datatype for column, datatype in columns_to_check.items() if column not in existing_columns}
+        for column, datatype in columns_to_add.items():
+            cursor.execute(f'ALTER TABLE ip_address ADD COLUMN {column} {datatype}')
+        conn.commit()
+    except Exception as e:
+        print(f"An error occurred while ensuring table schema: {e}")
+
 async def main():
     conn = sqlite3.connect(bans_db, check_same_thread=False)
-    cursor = conn.cursor()
-    cursor.execute('''CREATE TABLE IF NOT EXISTS ip_address
-                      (time TEXT, address TEXT UNIQUE, white TEXT, player_name TEXT)''')
-    conn.commit()
+    await ensure_table_schema(conn)
 
     if os.path.exists(log_file):
         print('Working...')
