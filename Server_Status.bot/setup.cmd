657fd90c123f90ebf65db4f8f9aed526f8f4c681
⎛⎝Иллидан⎠⎞ <88918931+JTNeXuS2@users.noreply.github.com>
2026-04-10 15:27:36 +0200

    Create setup.cmd

---
+++ Server_Status.bot/setup.cmd
@@ -0,0 +1,14 @@
+@echo off
+
+python.exe -m pip install --upgrade pip
+pip install -U disnake
+::pip install -U matplotlib
+pip3 install python-a2s
+pip install aiofiles
+pip install requests
+pip install psutil
+pip install telnetlib3
+
+pip install telnetlib-313-and-up
+
+timeout /t 5
